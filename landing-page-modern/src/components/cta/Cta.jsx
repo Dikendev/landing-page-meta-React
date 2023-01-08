5554132d1e0e38c7dc5fcfdb93ecc3491fa6660b
diken.dev <diken.dev@gmail.com>
2023-01-08 19:10:36 -0300

    added cta container and mobile responsive

--- landing-page-modern/src/components/cta/Cta.jsx
+++ landing-page-modern/src/components/cta/Cta.jsx
@@ -3,8 +3,14 @@ import './cta.css';
 
 function Cta() {
     return (
-        <div>
-            Call to action
+        <div className="meta__cta section__padding">
+            <div className="meta__meta-content">
+                <p>Request Early Access to Get</p>
+                <h3>Register today & start exploring the endless possibilities</h3>
+            </div>
+            <div className="meta__cta-btn">
+                <button type='button'>Get Started</button>
+            </div>
         </div>
     )
 }
